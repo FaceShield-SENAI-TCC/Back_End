4a15e515cbe24ef0d1acb704737d41384cd8f5dd
Alexandre Santos <alexandre.santana2201@gmail.com>
2025-10-02 14:44:07 -0300

    ainda tem q testar

--- FaceShield_Back/src/main/java/com/example/FaceShield_Back/Config/SecurityConfig.java
+++ FaceShield_Back/src/main/java/com/example/FaceShield_Back/Config/SecurityConfig.java
@@ -32,6 +32,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Se usar Swagger
+                        .requestMatchers("/").permitAll() // Libera a raiz
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
